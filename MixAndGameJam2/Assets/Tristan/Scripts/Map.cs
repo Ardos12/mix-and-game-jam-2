cd9644015aba2516f30104ce293c0b68cb58b733
Tristan <tristan.glandut@gmail.com>
2020-11-14 12:09:16 +0100

    Début de création de grille
    GetTile(x,y) pour récupérer la position d'une tile
    TileStat : propriété d'une tile

---
+++ MixAndGameJam2/Assets/Tristan/Scripts/Map.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public int sizeTile = 5;
+    GameObject[] tiles;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tiles = GameObject.FindGameObjectsWithTag("Tile");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject getTile(int x, int z)
+    {
+        GameObject toRet = null;
+
+        for(int i = 0; i < tiles.Length; i++)
+        {
+            if(tiles[i].transform.position.x / sizeTile == x && tiles[i].transform.position.z / sizeTile == z)
+            {
+                toRet = tiles[i];
+            }
+        }
+
+        return toRet;
+    }
+}
---
+++ MixAndGameJam2/Assets/Tristan/Scripts/TileStat.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileStat : MonoBehaviour
+{
+
+    public int visible = 1;
+    public int x = 0;
+    public int z = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
