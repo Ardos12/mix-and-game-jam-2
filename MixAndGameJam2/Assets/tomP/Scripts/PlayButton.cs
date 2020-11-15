a7a724dad4e6848ee681b7a89074e0b6adf71b29
Tom Parseihian <tom.parseihian@gmail.com>
2020-11-15 14:52:49 +0100

    Menu fonctionnel

---
+++ MixAndGameJam2/Assets/tomP/Scripts/PlayButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    public Sprite play;
+    public Sprite playSelected;
+
+    public Button btn;
+
+    void Start()
+    {
+        btn = GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void OnMouseOver()
+    {
+        GetComponent<Button>().image.sprite = playSelected;
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<Button>().image.sprite = play;
+    }
+}
---
+++ MixAndGameJam2/Assets/tomP/Scripts/QuitButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class QuitButton : MonoBehaviour
+{
+    public Sprite play;
+    public Sprite playSelected;
+
+    public Button btn;
+
+    void Start()
+    {
+        btn = GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Application.Quit();
+    }
+
+    private void OnMouseOver()
+    {
+        GetComponent<Button>().image.sprite = playSelected;
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<Button>().image.sprite = play;
+    }
+}
